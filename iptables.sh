9d40210b1a756762bfa693effc54e131f42002ca
thom <riboulet@gmail.com>
2009-04-30 20:54:51 +0200

    back to something simpler

--- iptables.sh
+++ iptables.sh
@@ -31,7 +31,7 @@ BOLD="\033[1m"
 ## EFFACEMENT DES ANCIENNES REGLES		 ## 
 ###################################################
 
-echo -en "${BOLD}${YELLOW}Effacement des anciennes regles :${NORMAL}"
+echo -en "${BOLD}${YELLOW}Erasing old rules :${NORMAL}"
 ${IPTABLES} -t filter -F INPUT
 ${IPTABLES} -t filter -F OUTPUT
 ${IPTABLES} -t filter -F FORWARD
@@ -47,7 +47,7 @@ echo -e "\t\t\t${GREEN}OK${NORMAL}"
 ## REMISE A ZERO DES CHAINES			 ##
 ###################################################
 
-echo -en "${BOLD}${YELLOW}Remise a zero des chaines :${NORMAL}"
+echo -en "${BOLD}${YELLOW}Reseting to zero :${NORMAL}"
 ${IPTABLES} -t filter -Z
 ${IPTABLES} -t nat    -Z
 ${IPTABLES} -t mangle -Z
@@ -57,7 +57,7 @@ echo -e "\t\t\t\t${GREEN}OK${NORMAL}"
 ## MISE EN PLACE DE LA POLITIQUE PAR DEFAUT	 ##
 ###################################################
 
-echo -en "${BOLD}${YELLOW}Mise en place de la polique par defaut :${NORMAL}"
+echo -en "${BOLD}${YELLOW}Default policy setup :${NORMAL}"
 ${IPTABLES} -t filter -P INPUT   DROP
 ${IPTABLES} -t filter -P OUTPUT  ACCEPT
 ${IPTABLES} -t filter -P FORWARD DROP
@@ -65,6 +65,7 @@ echo -e "\t\t${GREEN}OK${NORMAL}\n"
 
 
 # filtering
+echo -en "${BOLD}${YELLOW}Setting up the filters :${NORMAL}"
 ${IPTABLES} -A INPUT -i lo -j ACCEPT
 ${IPTABLES} -A INPUT -i ${EXT_IF} -j ACCEPT
 ${IPTABLES} -A INPUT -i ${EXT_IF} -s ${INT_NET} -j DROP
@@ -74,34 +75,25 @@ ${IPTABLES} -A INPUT -i ${EXT_IF} -d ${INT_NET} -j ACCEPT
 ${IPTABLES} -A INPUT -i ${INT_IF} -d ${EXT_NET} -j ACCEPT
 ${IPTABLES} -A INPUT -i ${INT_IF} -d ${INT_NET} -p udp -j ACCEPT
 
-
 ${IPTABLES} -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT  
+echo -e "\t\t\t\t${GREEN}OK${NORMAL}"
+
 
 # nat
+echo -en "${BOLD}${YELLOW}Setting up NAT :${NORMAL}"
 ${IPTABLES} -t nat -A POSTROUTING -s ${INT_NET} -o $EXT_IF -j MASQUERADE
 ${IPTABLES} -A FORWARD -i ${INT_IF} -s ${INT_NET} -o ${EXT_IF} -j ACCEPT
+echo -e "\t\t\t\t${GREEN}OK${NORMAL}"
 
 # optimisation
 #${IPTABLES} -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS -o $INTERNET --clamp-mss-to-pmtu
 
 # letting things out
+echo -en "${BOLD}${YELLOW}Letting things out :${NORMAL}"
 ${IPTABLES} -A OUTPUT -s ${INT_NET} -o ${EXT_IF} -j ACCEPT
 ${IPTABLES} -A OUTPUT -o ${EXT_IF} -j ACCEPT
 ${IPTABLES} -A OUTPUT -o ${INT_IF} -p udp -j ACCEPT
-
-###################################################
-## FUCK nimda and codered :)                     ##
-###################################################
-
-echo -e "${BOLD}${YELLOW}Protection contre Nimda et codered :${NORMAL}\t\t\t${GREEN}OK${NORMAL}"
-${IPTABLES} -I INPUT -j DROP -m string -p tcp -s 0.0.0.0/0 --string "c+dir"
-${IPTABLES} -I INPUT -j DROP -m string -p tcp -s 0.0.0.0/0 --string "c+tftp"
-${IPTABLES} -I INPUT -j DROP -m string -p tcp -s 0.0.0.0/0 --string "cmd.exe"
-${IPTABLES} -I INPUT -j DROP -m string -p tcp -s 0.0.0.0/0 --string "default.ida"
-${IPTABLES} -I FORWARD -j DROP -m string -p tcp -s 0.0.0.0/0 --string "c+dir"
-${IPTABLES} -I FORWARD -j DROP -m string -p tcp -s 0.0.0.0/0 --string "c+tftp"
-${IPTABLES} -I FORWARD -j DROP -m string -p tcp -s 0.0.0.0/0 --string "cmd.exe"
-${IPTABLES} -I FORWARD -j DROP -m string -p tcp -s 0.0.0.0/0 --string "default.ida"
+echo -e "\t\t\t\t${GREEN}OK${NORMAL}"
 
 # end, dropping what's left
 ${IPTABLES} -A INPUT -j DROP
